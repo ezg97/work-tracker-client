38b36099c6f137930c57d3e4361c728afa5f96a2
Elijah Guerrero <elijahguerrero97@gmail.com>
2020-09-14 01:51:07 -0500

    updated sorting filters, high to low , low to high, paid, and unpaid

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -63,7 +63,16 @@ class Main extends Component {
             }
             else this.setState({sortByAmount: ""});
         }
+    }
+
+    handleSortByPayment = (sort) => {
+        console.log('changed: ', sort);
         
+        if (sort !== '') {
+            this.setState({ sortByPayment: sort});
+        }
+        else this.setState({sortByPayment: ""});
+       
     }
 
     render() {
@@ -99,45 +108,166 @@ class Main extends Component {
                         <option value={'high'}>{"High to Low"}</option> 
                     </select>
 
-                    {folderId !== 2
+                    {(Number(folderId) !== 2)
                         ? null
                         : <select id='note_options'
                             value={this.state.sortByPayment} 
-                            onChange={(e) => this.handleSortByPayment(e.target.value, folderId)}>
+                            onChange={(e) => this.handleSortByPayment(e.target.value)}>
                                 <option value="">None</option>
                                 <option value={'paid'}>{"Paid"}</option> 
                                 <option value={'unpaid'}>{"Unpaid"}</option> 
                         </select>
-
                     }
 
-                    
                 </div>
 
                 {/* DISPLAY NOTES */}
                 <div className="main__notelist">
-                    {console.log('issue? ',this.state.sortBy)}
-                    {(notes.length > 0 && folderId === 2 && this.state.sortByPayment)
-                    ?   (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId))).map(note => {
-                            return (<Note key={note.id} 
-                                note={ 
-                                {...note, 
-                                sortBy: (Number(folderId) === 1) 
-                                    ? this.state.inventorySort 
-                                    : this.state.transactionSort} 
-                            } />)
-                        })
-                    : (notes.length > 0)
-                    ?   (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId))).map(note => {
-                            return (<Note key={note.id} 
-                                note={ 
-                                {...note, 
-                                sortBy: (Number(folderId) === 1) 
-                                    ? this.state.inventorySort 
-                                    : this.state.transactionSort} 
-                            } />)
-                        })
-                    :   <p>No inventory or purchases.</p>}
+                    {console.log('issue? ',this.state.sortByPayment)}
+                    {console.log('payment status', notes)}
+                    {console.log('testing algo: ',(notes.length > 0 && Number(folderId) === 2 && this.state.sortByPayment !== ""))}
+                    
+                    {/* 1) Has notes && 2) Folder: Transactions && 3) A sort by payment method (paid or unpaid) has been selected */}
+                    {(notes.length > 0 && Number(folderId) === 2 && this.state.sortByPayment !== "")
+                        // If "paid" was selected
+                        ? (this.state.sortByPayment === 'paid')
+                            // If sort by price option hasn't been selected "amount" 
+                            ? this.state.sortByAmount === ''
+                                ? (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                                        .filter(note => note.paymentStatus === true)
+                                                        .map(note => {
+                                                            return (<Note key={note.id} 
+                                                                note={ 
+                                                                {...note, 
+                                                                sortBy: (Number(folderId) === 1) 
+                                                                    ? this.state.inventorySort 
+                                                                    : this.state.transactionSort} 
+                                                            } />)
+                                })
+
+                                // Otherwise, sort by price option has been selected
+                                : this.state.sortByAmount === 'high'
+                                    //sort by High to Low
+                                    ? (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                        .filter(note => note.paymentStatus === true)
+                                        .sort((a, b) => Number(a.total) - Number(b.total))
+                                        .map(note => {
+                                            return (<Note key={note.id} 
+                                                note={ 
+                                                {...note, 
+                                                sortBy: (Number(folderId) === 1) 
+                                                    ? this.state.inventorySort 
+                                                    : this.state.transactionSort} 
+                                            } />)
+                                            
+                                        })
+
+                                    //sort by Low to High
+                                    : (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                        .filter(note => note.paymentStatus === true)
+                                        .sort((a, b) => Number(b.total) - Number(a.total))
+                                        .map(note => {
+                                            return (<Note key={note.id} 
+                                                note={ 
+                                                {...note, 
+                                                sortBy: (Number(folderId) === 1) 
+                                                    ? this.state.inventorySort 
+                                                    : this.state.transactionSort} 
+                                            } />)
+                                            
+                                        })
+
+                            // Otherwise, "UNpaid" was selected
+                            : this.state.sortByAmount === ''
+                                ? (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                    .filter(note => note.paymentStatus === false)
+                                    .map(note => {
+                                        return (<Note key={note.id} 
+                                            note={ 
+                                            {...note, 
+                                            sortBy: (Number(folderId) === 1) 
+                                                ? this.state.inventorySort 
+                                                : this.state.transactionSort} 
+                                        } />)
+                                    })
+                                // Otherwise, sort by price option has been selected
+                                : this.state.sortByAmount === 'high'
+                                    //sort by High to Low
+                                    ? (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                        .filter(note => note.paymentStatus === false)
+                                        .sort((a, b) => Number(b.total) - Number(a.total))
+                                        .map(note => {
+                                            return (<Note key={note.id} 
+                                                note={ 
+                                                {...note, 
+                                                sortBy: (Number(folderId) === 1) 
+                                                    ? this.state.inventorySort 
+                                                    : this.state.transactionSort} 
+                                            } />)
+                                            
+                                        })
+
+                                    //sort by Low to High
+                                    : (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                        .filter(note => note.paymentStatus === false)
+                                        .sort((a, b) => Number(a.total) - Number(b.total)) 
+                                        .map(note => {
+                                            return (<Note key={note.id} 
+                                                note={ 
+                                                {...note, 
+                                                sortBy: (Number(folderId) === 1) 
+                                                    ? this.state.inventorySort 
+                                                    : this.state.transactionSort} 
+                                            } />)
+                                            
+                                        })
+
+                    
+                        // WITHOUT paid and unpaid filter
+                        : (notes.length > 0)
+                            // If sort by price option or sort by quantity hasn't been selected "amount" 
+                            ? ((this.state.sortByQuantity === '' && Number(folderId) === 1) || this.state.sortByAmount === '' && Number(folderId) === 2)
+                                ? (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                                        .map(note => {
+                                                            return (<Note key={note.id} 
+                                                                note={ 
+                                                                {...note, 
+                                                                sortBy: (Number(folderId) === 1) 
+                                                                    ? this.state.inventorySort 
+                                                                    : this.state.transactionSort} 
+                                                            } />)
+                                })
+
+                                // Otherwise, sort by price option has been selected
+                                : ((this.state.sortByQuantity === 'high' && Number(folderId) === 1) || (this.state.sortByAmount === 'high' && Number(folderId) === 2))
+                                    //sort by High to Low
+                                    ? (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                        .sort((a, b) => (Number(folderId) === 1) ? (Number(b.quantity) - Number(a.quantity)) : (Number(b.total) - Number(a.total)))
+                                        .map(note => {
+                                            return (<Note key={note.id} 
+                                                note={ 
+                                                {...note, 
+                                                sortBy: (Number(folderId) === 1) 
+                                                    ? this.state.inventorySort 
+                                                    : this.state.transactionSort} 
+                                            } />)
+                                            
+                                        })
+
+                                    //sort by Low to High
+                                    : (!folderId ? notes : notes.filter(note => note.folderid === Number(folderId)))
+                                        .sort((a, b) => (Number(folderId) === 1) ? (Number(a.quantity) - Number(b.quantity)) : (Number(a.total) - Number(b.total)))
+                                        .map(note => {
+                                            return (<Note key={note.id} 
+                                                note={ 
+                                                {...note, 
+                                                sortBy: (Number(folderId) === 1) 
+                                                    ? this.state.inventorySort 
+                                                    : this.state.transactionSort} 
+                                            } />)
+                                            
+                                        })
+                            :   <p>No inventory or purchases.</p>}
                     {/* */}
                 </div>
 
